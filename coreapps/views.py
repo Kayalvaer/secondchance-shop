9e3906002365aa7de7778f4aa5bdebdab9a0e275
Kay Alvaer <62592167+Kayalvaer@users.noreply.github.com>
2021-08-27 10:22:30 +0200

    Add cart and checkout templates

--- coreapps/views.py
+++ coreapps/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def index(request):
     """ A view to return the index page """
-    return render(request, 'home/index.html')
+    context = {}
+    return render(request, 'home/index.html', context)
+
+def cart(request):
+    """ A view to return the cart page """
+    context = {}
+    return render(request, 'home/cart.html', context)
+
+def checkout(request):
+    """ A view to return the checkout page """
+    context = {}
+    return render(request, 'home/checkout.html', context)
