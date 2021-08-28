524a8304190d07ec4b4d8b492ea8d4a728245932
Kay Alvaer <62592167+Kayalvaer@users.noreply.github.com>
2021-08-28 19:47:49 +0200

    Added base code setup, home page page, footer elements

--- shopcore/settings.py
+++ shopcore/settings.py
@@ -148,6 +148,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR /'static'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'shopcore/static'),
